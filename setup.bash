5d1c6db2d8985134fb1b27cb855688d12f4e123f
Wolfram Fischer <Wolfram.Fischer1979@gmail.com>
2021-08-20 09:34:56 +0200

    Rename virtualenv directory to .venv

--- setup.bash
+++ setup.bash
@@ -4,7 +4,7 @@ set -e
 ln -sf ../../git_hooks/pre-commit .git/hooks/pre-commit
 chmod +x .git/hooks/pre-commit
 
-python3.9 -m venv --prompt python-template venv
-source venv/bin/activate
+python3.9 -m venv --prompt python-template .venv
+source .venv/bin/activate
 python3.9 -m pip install --upgrade pip
 pip install --upgrade --upgrade-strategy eager -r requirements.txt
