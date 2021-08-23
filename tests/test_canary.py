4025570536ddba99a765f559bf25a97cc56f4f84
Wolfram Fischer <Wolfram.Fischer1979@gmail.com>
2021-08-23 10:55:58 +0200

    Use flake8 for unit tests and add missing documentation

--- tests/test_canary.py
+++ tests/test_canary.py
@@ -1,8 +1,12 @@
+"""Canary unit test."""
 import unittest
 
 
 class TestCanaray(unittest.TestCase):
+    """Canary test class."""
+
     def test_canary(self) -> None:
+        """Canary."""
         self.assertTrue(True)
 
 
