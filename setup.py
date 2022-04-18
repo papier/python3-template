ae6a7b988997968b7b4443903808ec1bc5bee53b
Wolfram Fischer <Wolfram.Fischer1979@gmail.com>
2022-04-18 03:54:49 +0200

    Move setup.py to pyproject.toml

--- setup.py
+++
@@ -1,9 +0,0 @@
-import setuptools
-
-setuptools.setup(
-    name="project",
-    version="1.0.0",
-    description="Python3 sample project",
-    packages=setuptools.find_packages(where="src"),
-    package_dir={"": "src"},
-)
